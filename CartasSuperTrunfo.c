32c0cd688e5ac4bcad93623308a6413a06f04b17
MLopes1000 <marcelo.olavo@hotmail.com>
2026-03-18 17:48:57 +0000

    Fiz o desafio nível novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,59 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  
+    char estado;
+    char codigoDaCarta, nomeDaCidade[ 50 ];
+    int populacao, numeroDePontosTuristicos;
+    float area, pib;
 
-  // Área para entrada de dados
+    printf("Carta 1\n");
 
-  // Área para exibição dos dados da cidade
+    printf("Estado: ");
+    scanf("%s", &estado);
+
+    printf("código: ");
+    scanf("%s", &codigoDaCarta);
+
+    printf("Nome da cidade: ");
+    scanf("%s", &nomeDaCidade);
+
+    printf("População: ");
+    scanf("%d", &populacao);
+
+    printf("Área: ");
+    scanf("%f", &area);
+
+     printf("Pib: ");
+    scanf("%f", &pib);
+
+     printf("Número de pontos turísticos: ");
+    scanf("%f " , &numeroDePontosTuristicos  );
+    
+
+
+     printf("Carta 2\n");
+
+    printf("Estado: ");
+    scanf("%s", &estado);
+
+    printf("código: ");
+    scanf("%s", &codigoDaCarta);
+
+    printf("Nome da cidade: ");
+    scanf("%s", &nomeDaCidade);
+
+    printf("População: ");
+    scanf("%d", &populacao);
+
+    printf("Área: ");
+    scanf("%f", &area);
+
+     printf("Pib: ");
+    scanf("%f", &pib);
+
+     printf("Número de pontos turísticos: ");
+    scanf("%f", &numeroDePontosTuristicos);
 
 return 0;
 } 
